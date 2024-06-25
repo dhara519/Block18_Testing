c98f6d72f4984dd6ed1ca5d18a0e12f31a92929d
dhara519 <dhara519@gmail.com>
2024-06-25 16:54:55 -0400

    submitting assignment

---
+++ testing.js
@@ -0,0 +1,21 @@
+// 1) Expectation: Running the multiplication function should return the product of the two inputted numbers.
+
+// Expect multiply(5,3) to return 15
+// Expect multiply(a,3) to return an error
+// Expect multiply(3) to return an error
+// Expect multiply("3",9) to return an error
+// Expect multiply(0,0) to return a number
+
+// 2) Expectation: Running the ConcatOdds function should return an array of numbers not divisble by 2
+// Expect concatOdds([],[]) to return an empty array
+// Expect concatOdds([],[9,9,0]) to return an array of [0,9]
+// Expect concatOdds([two, three,four],[5,6,7]) to return an error
+// Expect concatOdds([5,5,5], [9,3,4]) to return [3,5,9]
+// Expect concatOdds([1.3, 90, -1],[57]) to return an error
+// Expect concatOdds([--5,9],[20,6]) to return an error
+
+// 3) Expectation: if the cart is empty during checkout. User should be sent an alert stating their cart is empty and prompted if they want to log into their account. Below the alert there should be links to the landing page of the main shopping categories of the website.
+//  Expect: When the user enters incorrect log in info, they should be prompted to create an account or checkout as a guest
+//  Expect: At each step of checkout the user's name and cart total should display.
+//  Expect: When the user choses guest checkout, they should be prompted for their name and email address
+//  Expect: When the user enters incorrect log in info, they should be prompted to create an account or checkout as a guest
